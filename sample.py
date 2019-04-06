bcb5ddc4ead836795041adf8925e4f2c179fbba5
Avene <juc-y@live.jp>
2019-04-06 15:52:45 +0000

    print top5 for a keyword

--- sample.py
+++ sample.py
@@ -1,6 +1,16 @@
+import sys
+import urllib.parse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+def get_top_product_links(keyword):
+    url = 'https://www.buyma.com/r/' + keyword
+    html = urlopen(url)
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    products = bs.find_all('div', {'class':'product_name'})
+    links = ['https://www.buyma.com' + product.find('a', {'data-action':'view_item'}).attrs['href'] for product in products]
+    return links
+    
 def find_title(detail_page):
     title = detail_page.find('h1', id="item_h1").find('span', {'itemprop':"name"}).get_text()
     return title
@@ -21,13 +31,19 @@ def find_price(detail_page):
     price = detail_page.find('div', id="priceWrap").find('span', {'class':'price_txt'}).get_text()
     return price
 
-# html = urlopen('https://www.buyma.com/r/%E3%83%A2%E3%83%B3%E3%82%AF%E3%83%AC%E3%83%BC%E3%83%AB/')
-html = urlopen('https://www.buyma.com/item/41422450/')
-bs = BeautifulSoup(html.read(), 'html.parser')
+def print_detail(bs):
+    print(find_title(bs),end=',')
+    print(find_inq_count(bs), end=',')
+    print(find_access_count(bs), end=',')
+    print(find_fav_count(bs), end=',')
+    print(find_price(bs), end=',')
+    print()
 
-print(find_title(bs),end=',')
-print(find_inq_count(bs), end=',')
-print(find_access_count(bs), end=',')
-print(find_fav_count(bs), end=',')
-print(find_price(bs), end=',')
-print()
+links = get_top_product_links(urllib.parse.quote(sys.argv[1]))
+links = links[0:5]
+print(len(links), 'items found')
+detail_pages = [urlopen(link) for link in links]
+detail_bs_objs = [BeautifulSoup(detail.read(), 'html.parser') for detail in detail_pages]
+
+for detail_obj in detail_bs_objs:
+    print_detail(detail_obj)
